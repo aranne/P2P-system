3188326c42662575807a0027e0c34203edf9fea0
Weiran Fu <fuweiran816@gmail.com>
2020-11-08 22:13:29 -0500

    add message generator

---
+++ src/ProtocolGenerator.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+
+/*
+    Peer to Server Protocol
+
+    Request:
+    method <sp> RFC number <sp> version <cr> <lf> header field name <sp> value <cr> <lf>
+    header field name <sp> value <cr> <lf>
+    <cr> <lf>
+
+    Response:
+    version <sp> status code <sp> phrase <cr> <lf>
+    <cr> <lf>
+    RFC number <sp> RFC title <sp> hostname <sp> upload port number<cr><lf>
+    RFC number <sp> RFC title <sp> hostname <sp> upload port number<cr><lf> ...
+    <cr> <lf>
+ */
+public class ProtocolGenerator {
+    enum Method {
+        GET,
+        ADD,
+        LOOKUP,
+        LIST
+    }
+    private static final String VERSION = "P2P-CI/1.0";
+    private static final String CRLF = "\r\n";
+
+    public static void generateRequest(OutputStream out, Method method, Map<String, String> headers) throws ProtocolFormatException, IOException {
+        if (method == Method.LIST) {
+            generateRequest(out, method, -1, headers);
+        } else {
+            throw new ProtocolFormatException("Not supported method: " + method);
+        }
+    }
+
+    public static void generateRequest(OutputStream out, Method method, int RFCNum, Map<String, String> headers) throws ProtocolFormatException, IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(out, StandardCharsets.UTF_8));
+        if (method == Method.LIST) {
+            bw.write(method.toString() + " RFC ALL " + VERSION + CRLF);
+        } else if (method == Method.GET || method == Method.ADD || method == Method.LOOKUP) {
+            bw.write(method.toString() + " RFC " + RFCNum + " " + VERSION + CRLF);
+        } else {
+            throw new ProtocolFormatException("Not supported method: " + method);
+        }
+        for (String header : headers.keySet()) {
+            bw.write(header + ": " + headers.get(header) + CRLF);
+        }
+        bw.write(CRLF);
+        bw.flush();
+    }
+
+    public static void generateResponse(OutputStream out, int statusCode, String status, Map<String, String> headers, Path file) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(out, StandardCharsets.UTF_8));
+        bw.write(VERSION + " " + statusCode + " " + status + CRLF);
+        for (String header : headers.keySet()) {
+            bw.write(header + ": " + headers.get(header) + CRLF);
+        }
+        bw.write(CRLF);
+        bw.flush();                                          // flush into stream before reading file
+        Files.copy(file, out);
+        out.flush();
+        bw.write(CRLF);
+        bw.flush();
+    }
+
+    public static void generateResponse(OutputStream out, int statusCode, String status, List<String> RFCRecords) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(out, StandardCharsets.UTF_8));
+        bw.write(VERSION + " " + statusCode + " " + status + CRLF);
+        bw.write(CRLF);
+        for (String RFCRecord : RFCRecords) {
+            bw.write(RFCRecord + CRLF);
+        }
+        bw.write(CRLF);
+        bw.flush();
+    }
+}
