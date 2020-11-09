df2170a776b535b01bb8b1e17117d369accc2f4b
Weiran Fu <fuweiran816@gmail.com>
2020-11-09 12:45:03 -0500

    upload RFC

--- src/PeerClient.java
+++ src/PeerClient.java
@@ -5,28 +5,28 @@ import java.util.*;
 
 public class PeerClient {
     private int SERVER_PORT = 7734;
-    private int LOCAL_PORT;
+    private final int UPLOAD_LOCAL_PORT;
     private final String SERVER_HOSTNAME;
     private String LOCAL_HOSTNAME;
-    private Socket socket = null;
     private OutputStream out = null;
     private boolean isClosed = false;
-    private List<RFC> RFCList = new ArrayList<>();
+    private final List<RFC> RFCList = new ArrayList<>();
 
-    public PeerClient(String serverHostname) {
+    public PeerClient(String serverHostname, int uploadPort) {
         this.SERVER_HOSTNAME = serverHostname;
+        this.UPLOAD_LOCAL_PORT = uploadPort;
     }
 
-    public PeerClient(String serverHostname, int serverPort) {
+    public PeerClient(String serverHostname, int serverPort, int uploadPort) {
         this.SERVER_HOSTNAME = serverHostname;
         this.SERVER_PORT = serverPort;
+        this.UPLOAD_LOCAL_PORT = uploadPort;
     }
 
     public void start() {
         try {
-            socket = new Socket(SERVER_HOSTNAME, SERVER_PORT);
+            Socket socket = new Socket(SERVER_HOSTNAME, SERVER_PORT);
             out = socket.getOutputStream();
-            LOCAL_PORT = socket.getLocalPort();
             LOCAL_HOSTNAME = socket.getLocalAddress().getHostName();
         } catch (IOException e) {
             throw new RuntimeException("Cannot connect to server at " + SERVER_HOSTNAME + " on port " + SERVER_PORT);
@@ -35,14 +35,18 @@ public class PeerClient {
 
     public void addRFC(RFC rfc) {
         RFCList.add(rfc);
+        uploadRFC(rfc);
+    }
+
+    public void uploadRFC(RFC rfc) {
         LinkedHashMap<String, String> headers = new LinkedHashMap<>();
         headers.put("Host", LOCAL_HOSTNAME);
-        headers.put("Port", String.valueOf(LOCAL_PORT));
+        headers.put("Port", String.valueOf(UPLOAD_LOCAL_PORT));
         headers.put("Title", rfc.getTitle());
         try {
             MessageGenerator.generateRequest(out, MessageGenerator.Method.ADD, rfc.getRFCNum(), headers);
         } catch (MessageFormatException | IOException e) {
-            System.out.println("Error in adding rfc: " + rfc);
+            System.out.println("Error uploading rfc: " + rfc);
             RFCList.remove(rfc);
         }
     }
