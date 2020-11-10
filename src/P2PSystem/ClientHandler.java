0b86416162a4c6994ea0d066393b883a86471d05
Weiran Fu <fuweiran816@gmail.com>
2020-11-09 23:27:29 -0500

    handle Bad Request

--- src/P2PSystem/ClientHandler.java
+++ src/P2PSystem/ClientHandler.java
@@ -3,7 +3,9 @@ package P2PSystem;
 import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 
 public class ClientHandler implements Runnable {
 
@@ -11,6 +13,9 @@ public class ClientHandler implements Runnable {
     private final CentralServerData centralServerData;
     private final String LOCAL_HOSTNAME;
     private CentralServerData.PeerInfo currentPeer = null;
+    private OutputStream out = null;
+    private static final String VERSION = "P2P-CI/1.0";
+    private static final int ALL = -1;
 
     public ClientHandler(Socket clientSocket, CentralServerData centralServerData) {
         this.socket = clientSocket;
@@ -24,6 +29,7 @@ public class ClientHandler implements Runnable {
         StringBuilder sb = new StringBuilder();
         try {
             InputStream in = socket.getInputStream();
+            out = socket.getOutputStream();
             BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
             String line;
             while ((line = br.readLine()) != null) { // end of stream
@@ -36,47 +42,145 @@ public class ClientHandler implements Runnable {
                 }
             }
             in.close();
+            out.close();
             socket.close();
-            System.out.println("Socket is closed with: " + LOCAL_HOSTNAME);
+            System.out.println("Socket is closed with " + LOCAL_HOSTNAME);
         } catch (IOException | MessageFormatException e) {
-            System.out.println("IO Error in server thread");
+            System.out.println("IO error in central server thread");
         } finally {
             if (currentPeer != null) {
                 centralServerData.removePeerInfo(currentPeer);
                 centralServerData.removeRFCIndices(currentPeer);
             }
+            System.out.println("After leaving "+ centralServerData);
         }
     }
 
     private void handleMessage(MessageParser.Message message) {
         String[] requestLine = message.requestLine.split(" ");
-        MessageGenerator.Method method = Enum.valueOf(MessageGenerator.Method.class, requestLine[0]);
-        LinkedHashMap<String, String> headers = message.headers;
-        String hostName = headers.get("Host");
-        int uploadPort = Integer.parseInt(headers.get("Port"));
+        if (requestLine.length != 4) {
+            handleBadRequest();
+            return;
+        }
+        MessageGenerator.Method method;
+        try {
+            method = Enum.valueOf(MessageGenerator.Method.class, requestLine[0]);
+        } catch (IllegalArgumentException e) {
+            handleBadRequest();
+            return;
+        }
         int RFCNum;
         try {
             RFCNum = Integer.parseInt(requestLine[2]);
         } catch (NumberFormatException e) {
-            RFCNum = -1;
+            if (requestLine[2].equals("ALL")) RFCNum = ALL;
+            else {
+                handleBadRequest();
+                return;
+            }
         }
-        String title = headers.getOrDefault("Title", "");
+        String P2PVersion = requestLine[3];
+        if (!P2PVersion.equals(VERSION)) {
+            handleNotSupportedVersion();
+            return;
+        }
+        LinkedHashMap<String, String> headers = message.headers;
+        String hostName = headers.get("Host");
+        if (hostName == null) {
+            handleBadRequest();
+            return;
+        }
+        int uploadPort;
+        try {
+            uploadPort = Integer.parseInt(headers.get("Port"));
+        } catch (NumberFormatException e) {
+            handleBadRequest();
+            return;
+        }
+
+        String title = headers.get("Title");
+        if (title == null && RFCNum != ALL) {
+            handleBadRequest();
+            return;
+        }
+        currentPeer = new CentralServerData.PeerInfo(hostName, uploadPort);
         switch (method) {
             case ADD:
                 CentralServerData.RFCIndex rfcIndex = new CentralServerData.RFCIndex(RFCNum, title, hostName);
-                currentPeer = new CentralServerData.PeerInfo(hostName, uploadPort);
-                handleAdd(currentPeer, rfcIndex);
+                handleAdd(rfcIndex);
                 break;
             case LOOKUP:
+                RFC rfc = new RFC(RFCNum, title);
+                handleLookUp(rfc);
                 break;
             case LIST:
-
+                handleList();
+                break;
         }
     }
 
-    private void handleAdd(CentralServerData.PeerInfo currentPeer, CentralServerData.RFCIndex rfcIndex) {
+    private void handleBadRequest() {
+        List<String> RFCRecords = new ArrayList<>();
+        try {
+            MessageGenerator.generateResponse(out, 400, "Bad Request", RFCRecords);
+        } catch (IOException e) {
+            System.out.println("Error sending response to client");
+        }
+    }
+
+    private void handleNotSupportedVersion() {
+        List<String> RFCRecords = new ArrayList<>();
+        try {
+            MessageGenerator.generateResponse(out, 505, "P2P-CI Version Not Supported", RFCRecords);
+        } catch (IOException e) {
+            System.out.println("Error sending response to client");
+        }
+    }
+
+    private void handleAdd(CentralServerData.RFCIndex rfcIndex) {
         centralServerData.addPeerInfo(currentPeer);
         centralServerData.addRFCIndex(rfcIndex);
-        System.out.println("DEBUG "+ centralServerData);
+        System.out.println("After adding "+ centralServerData);
+
+        List<String> RFCRecords = new ArrayList<>();
+        String RFCRecord = MessageGenerator.generateRFCResponseFormat(rfcIndex.getRFCNum(), rfcIndex.getTitle(), rfcIndex.getHostname(), currentPeer.getUploadPort());
+        RFCRecords.add(RFCRecord);
+        try {
+            MessageGenerator.generateResponse(out, 200, "OK", RFCRecords);
+        } catch (IOException e) {
+            System.out.println("Error sending response to Add request from " + currentPeer.getHostname());
+        }
+    }
+
+    private void handleLookUp(RFC rfc) {
+        List<CentralServerData.RFCIndex> RFCIndices = centralServerData.lookupRFCIndices(rfc);
+        List<String> RFCRecords = new ArrayList<>();
+        for  (CentralServerData.RFCIndex  rfcIndex : RFCIndices) {
+            String RFCRecord = MessageGenerator.generateRFCResponseFormat(rfcIndex.getRFCNum(), rfcIndex.getTitle(), rfcIndex.getHostname(), currentPeer.getUploadPort());
+            RFCRecords.add(RFCRecord);
+        }
+        try {
+            if (!RFCRecords.isEmpty()) {
+                MessageGenerator.generateResponse(out, 200, "OK", RFCRecords);
+            } else {
+                MessageGenerator.generateResponse(out, 404, "Not Found", RFCRecords);
+            }
+        } catch (IOException e) {
+            System.out.println("Error sending response to LookUp request from " + currentPeer.getHostname());
+        }
+    }
+
+    private void handleList() {
+        List<CentralServerData.RFCIndex> RFCIndices = centralServerData.listRFCIndices();
+        List<String> RFCRecords = new ArrayList<>();
+        for  (CentralServerData.RFCIndex  rfcIndex : RFCIndices) {
+            String RFCRecord = MessageGenerator.generateRFCResponseFormat(rfcIndex.getRFCNum(), rfcIndex.getTitle(), rfcIndex.getHostname(), currentPeer.getUploadPort());
+            RFCRecords.add(RFCRecord);
+        }
+        try {
+            MessageGenerator.generateResponse(out, 200, "OK", RFCRecords);
+        } catch (IOException e) {
+            System.out.println("Error sending response to List request from " + currentPeer.getHostname());
+        }
     }
 }
