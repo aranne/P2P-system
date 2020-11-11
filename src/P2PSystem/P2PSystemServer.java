3ec0ca6abdc3c96f63082c5a8d5fb2008ef72db1
Weiran Fu <fuweiran816@gmail.com>
2020-11-10 23:03:50 -0500

    rename

--- src/P2PSystem/P2PSystemServer.java
+++ src/P2PSystem/P2PSystemServer.java
@@ -1,6 +1,6 @@
 package P2PSystem;
 
-public class TestServer {
+public class P2PSystemServer {
     public static void main(String[] args) {
         Server server = new Server(Server.Type.CENTRAL_SERVER);
         server.run();
